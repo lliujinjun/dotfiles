e3b44042934d6d469c2b6eb9e0d1c21592b1e59e
Jinjun Liu <lliujinjun@163.com>
2022-10-02 14:15:13 +0000

    feat(install.sh): add lxd for manager containers

--- install.sh
+++ install.sh
@@ -23,6 +23,9 @@ sudo pacman --sync --needed --noconfirm --noprogressbar kitty-terminfo &>/dev/nu
 sudo pacman --sync --needed --noconfirm --noprogressbar podman &>/dev/null
 # install fzf package
 sudo pacman --sync --needed --noconfirm --noprogressbar fzf &>/dev/null
+# install lxd package and dependencies
+sudo pacman --remove --nodeps --nodeps --noconfirm --noprogressbar --nosave iptables &>/dev/null # iptables-nft and iptables are in conflict
+sudo pacman --sync --needed --noconfirm --noprogressbar lxd &>/dev/null
 
 # run rootless podman
 sudo touch /etc/subuid /etc/subgid
@@ -38,3 +41,23 @@ ssh-keygen -q -b 521 -C "lliujinjun@163.com" -f $HOME/.ssh/id_ecdsa -N '' -t ecd
 
 # start emacs daemon with systemd
 systemctl enable --user --now emacs 2>/dev/null
+
+# setup lxd
+# allow user namespace idmap range
+echo root:100000:65536 | sudo tee -a /etc/subuid /etc/subgid >/dev/null
+# start lxd daemon with systemd
+sudo systemctl enable --now lxd 2>/dev/null
+# allow unprivileged user access lxd
+sudo usermod -a -G lxd $USER
+# create storage pool
+sudo lxc storage create default dir &>/dev/null
+# add root device in the default profile
+sudo lxc profile device add default root disk path=/ pool=default &>/dev/null
+# create a new network
+sudo lxc network create lxdbr0 ipv4.address=10.10.10.1/24 &>/dev/null
+# add the interface to default profile
+sudo lxc profile device add default eth0 nic nictype=bridged parent=lxdbr0 &>/dev/null
+# start local application network name resolution
+sudo systemctl enable --now systemd-resolved 2>/dev/null
+# access container by name of the host
+sudo systemd-resolve --interface lxdbr0 --set-domain '~lxd' --set-dns $(sudo lxc network get lxdbr0 ipv4.address | cut -d / -f 1)
